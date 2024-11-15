aee993eb4d35f903d83ffa11b951e7b709ade111
leejimin <jimin980915@naver.com>
2024-11-15 23:26:44 +0900

    공부 5일차

--- sw_expert_academy/1206/1206.py
+++ sw_expert_academy/1206/1206.py
@@ -9,13 +9,13 @@ for test_case in range(1,T+1):
     N = int(input())
     buildings = list(map(int, input().split()))
     available = 0
-    for i in range(2, N - 2):
-        compare_lists = buildings[i-2: i+3] # i-2 부터 i + 2까지
-        compare_lists.remove(buildings[i]) #기준 삭제
-        
-        max_height = max(compare_lists) #비교군중에 제일 높은 건물 찾기
-        if max_height < buildings[i]: #제일 높은 건물이 기준 건물보다 낮으면
-            available += buildings[i] - max_height #조망권확보 : 반영
-    
-        
+
+    for i in range(2,N-2): # 첫번째, 마지막 두칸은 고려 x
+        compare_buildings = buildings[i-2: i+3] #좌 우 두칸씩
+        compare_buildings.remove(buildings[i])
+
+        max_building = max(compare_buildings)
+        if max_building < buildings[i]:
+            available += buildings[i] - max_building
+
     print(f'#{test_case} {available}')
